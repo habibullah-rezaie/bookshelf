607f54545658c503dce44f0b9e0793270009011d
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-23 05:20:35 +0430

    Create the 'App' itself

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Logo from "./components/logo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Logo />
+      <h1>Bookshelf</h1>
+      <button>Login</button>
+      <button>Register</button>
+    </>
   );
 }
 
