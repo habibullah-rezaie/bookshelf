9cf45659de590ea0ddd9bdbc8d6dca6918d443a5
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-25 01:40:07 +0430

    Create helper component 'Stack'

--- src/components/lib/Layout.tsx
+++ src/components/lib/Layout.tsx
@@ -6,3 +6,24 @@ type ContainerProps = React.PropsWithChildren<
 export function Container({ children, className }: ContainerProps) {
   return <div className={`w-full h-full ${className}`}>{children}</div>;
 }
+
+export function Stack({
+  gap = 1,
+  direction = "horizontal",
+  children,
+  className = "",
+}: PropsWithChildren<{
+  gap?: number;
+  direction?: "horizontal" | "vertical";
+  className?: string;
+}>) {
+  return (
+    <div
+      className={`grid grid-flow-${
+        direction === "horizontal" ? "col" : "row"
+      } gap-${gap} ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
