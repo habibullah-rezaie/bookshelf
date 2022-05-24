cbdf4b72da7a488e8e5601dcf092ef0a33272134
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-24 08:12:32 +0430

    Implement 'Button'

---
+++ src/components/lib/Buttons.tsx
@@ -0,0 +1,35 @@
+import React, { HTMLAttributes } from "react";
+
+type ButtonVariants = "primary" | "secondary" | "danger";
+
+interface CustomButtonProps {
+  variant: ButtonVariants;
+}
+
+type ButtonProps = React.PropsWithChildren<
+  CustomButtonProps &
+    React.DetailedHTMLProps<
+      React.ButtonHTMLAttributes<HTMLButtonElement>,
+      HTMLButtonElement
+    >
+>;
+
+export function Button({
+  variant = "primary",
+  children,
+  className,
+}: ButtonProps) {
+  const variantStyles = {
+    primary: `text-baseColor hover:ring-indigo bg-indigo`,
+    secondary: `text-gray80 hover:ring-gray bg-gray`,
+    danger: ``,
+  };
+
+  return (
+    <button
+      className={`px-5 py-2 rounded-sm hover:ring-3 ${variantStyles[variant]} ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
