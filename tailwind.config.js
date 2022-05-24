a3bd7dd9afd58fdae94c07f63a3985dd6c620e0f
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-25 01:25:54 +0430

    Make colors extend defaults not overwrite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      baseColor: "white",
-      textColor: "#434449",
-      gray: "#f1f2f7",
-      gray10: "#f1f1f4",
-      gray20: "#e4e5e9",
-      gray80: "#6f7077",
-      indigo: "#3f51b5",
-      indigoDarken10: "#364495",
-      indigoLighten80: "#b7c1f8",
+    extend: {
+      colors: {
+        baseColor: "white",
+        textColor: "#434449",
+        gray: "#f1f2f7",
+        gray10: "#f1f1f4",
+        gray20: "#e4e5e9",
+        gray80: "#6f7077",
+        indigo: "#3f51b5",
+        indigoDarken10: "#364495",
+        indigoLighten80: "#b7c1f8",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
