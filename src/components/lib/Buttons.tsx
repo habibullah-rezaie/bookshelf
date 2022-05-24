00a1952e003dbce3bed8f17396a567bc43577fa3
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-25 01:24:20 +0430

    Make buttons look better

--- src/components/lib/Buttons.tsx
+++ src/components/lib/Buttons.tsx
@@ -21,14 +21,14 @@ export function Button({
   ...props
 }: ButtonProps) {
   const variantStyles = {
-    primary: `text-baseColor hover:ring-indigo bg-indigo`,
-    secondary: `text-gray80 hover:ring-gray bg-gray`,
+    primary: `text-baseColor bg-indigo ring-indigoDarken10`,
+    secondary: `text-gray80 ring-gray20 bg-gray`,
     danger: ``,
   };
 
   return (
     <button
-      className={`px-5 py-2 rounded-sm hover:ring-3 ${variantStyles[variant]} ${className}`}
+      className={`px-5 py-2 rounded-sm hover:ring-2 ring-offset-1 ring-opacity-50 ${variantStyles[variant]} ${className}`}
       {...props}
     >
       {children}
