3ec9e3918efe88d33be5a345c446efe481256dfe
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-29 11:58:27 +0430

    Rename file to match the name of the screen

--- src/__tests__/LoginRegisterScreen.test.tsx
+++ src/__tests__/LoginRegisterScreen.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import * as React from "react";
-import App from "../App";
+import App from "screens/LoginRegisterScreen";
 
 describe("App component", () => {
   let root: HTMLElement;
