82e559148dd579c325937b3df17b1b249a3caac4
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-06-02 10:25:24 +0430

    Implement Custom Input
    Accepts all native input props
    Accepts a ref

--- src/components/lib/Forms.tsx
+++ src/components/lib/Forms.tsx
@@ -24,3 +24,28 @@ export function FormGroup({
     <div className={`grid grid-flow-col gap-3 ${className}`}>{children}</div>
   );
 }
+
+type InputProps = PropsWithChildren<
+  { ring?: boolean } & Omit<
+    React.DetailedHTMLProps<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    "ref"
+  >
+>;
+
+export const Input = React.forwardRef(function Input(
+  { className = "", ring = false, ...props }: InputProps,
+  ref: React.ForwardedRef<HTMLInputElement>
+) {
+  return (
+    <input
+      ref={ref}
+      className={`bg-white p-1 outline-hidden outline-0 focus:outline-0 focus:outline-hidden rounded-md ${
+        ring ? `ring-2 ring-opacity-40 ring-lightGray focus:ring-4` : ""
+      } ${className}`}
+      {...props}
+    />
+  );
+});
