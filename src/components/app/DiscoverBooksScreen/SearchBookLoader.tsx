133c18eab032556a9cdc851fabb3087e79fce6a4
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-06-11 02:09:18 +0430

    Create SearchBookLoader.tsx

---
+++ src/components/app/DiscoverBooksScreen/SearchBookLoader.tsx
@@ -0,0 +1,12 @@
+import BookCardSkeletonLoader from "./BookCardSkeletonLoader";
+
+function SerachBookLoader() {
+  return (
+    <>
+      <BookCardSkeletonLoader />
+      <BookCardSkeletonLoader />
+    </>
+  );
+}
+
+export default SerachBookLoader;
