b56eda2c8625c3ff72377382dfd3b4e440b8e0b6
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-06-11 02:13:45 +0430

    Move 'isDeepStrictEqual' to utils directory

--- src/components/app/DiscoverBooksScreen/SearchBooks.tsx
+++ src/components/app/DiscoverBooksScreen/SearchBooks.tsx
@@ -5,6 +5,7 @@ import React, { FormEvent, useEffect, useState } from "react";
 import { FaFilter, FaSearch, FaSpinner } from "react-icons/fa";
 import { SearchFilters, SearchResult } from "types/DiscoverBooksScreenTypes";
 import { BaseComponentStatuses } from "types/types";
+import { isDeepStrictEqual } from "utils/utils";
 const FiltersModal = React.lazy(
   () =>
     import(
@@ -205,31 +206,4 @@ function searchBook(term: string, filters?: SearchFilters) {
   );
 }
 
-function isDeepStrictEqual(
-  obj1: { [key: string]: any },
-  obj2: { [key: string]: any }
-) {
-  const obj1Props = Object.getOwnPropertyNames(obj1);
-  const obj2Props = Object.getOwnPropertyNames(obj2);
-
-  // has same number of keys
-  if (obj1Props.length !== obj2Props.length) return false;
-
-  for (const key of obj1Props) {
-    // has same keys
-    if (!(key in obj2)) return false;
-    if (typeof obj1[key] !== typeof obj2[key]) return false;
-  }
-
-  for (const key of obj1Props) {
-    // keys has same values
-    if (typeof obj1[key] !== "object") {
-      if (obj1[key] !== obj2[key]) return false;
-    } else {
-      if (!isDeepStrictEqual(obj1[key], obj2[key])) return false;
-    }
-  }
-
-  return true;
-}
 export default SearchBooks;
---
+++ src/utils/utils.ts
@@ -0,0 +1,27 @@
+export function isDeepStrictEqual(
+  obj1: { [key: string]: any },
+  obj2: { [key: string]: any }
+) {
+  const obj1Props = Object.getOwnPropertyNames(obj1);
+  const obj2Props = Object.getOwnPropertyNames(obj2);
+
+  // has same number of keys
+  if (obj1Props.length !== obj2Props.length) return false;
+
+  for (const key of obj1Props) {
+    // has same keys
+    if (!(key in obj2)) return false;
+    if (typeof obj1[key] !== typeof obj2[key]) return false;
+  }
+
+  for (const key of obj1Props) {
+    // keys has same values
+    if (typeof obj1[key] !== "object") {
+      if (obj1[key] !== obj2[key]) return false;
+    } else {
+      if (!isDeepStrictEqual(obj1[key], obj2[key])) return false;
+    }
+  }
+
+  return true;
+}
