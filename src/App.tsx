acddef9d2c0c343a07c040dd2a88a52d1d0a77c8
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-06-15 19:28:29 +0430

    For now do not require login

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import DiscoverBooksScreen from "screens/DiscoverBooksScreen";
 import LoginRegisterScreen from "screens/LoginRegisterScreen";
 
 function App() {
-  const user = null;
+  const user = { password: "mypassword", username: "GUEST" };
+  // const user = null;
   return <>{user ? <DiscoverBooksScreen /> : <LoginRegisterScreen />}</>;
 }
 
