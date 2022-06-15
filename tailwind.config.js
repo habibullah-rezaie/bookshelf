a9113f6505946b8f388da602404d4226a271d884
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-06-15 19:28:46 +0430

    Add some base colors and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+const tlTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
   theme: {
     extend: {
+      screens: {
+        xxs: "320px",
+        xsm: "420px",
+        ...tlTheme.screens,
+      },
       colors: {
-        baseColor: "white",
+        baseColor: "3f51b5",
+        secondaryColor: "#F9B713",
         textColor: "#434449",
         baseGray: "#f1f2f7",
         lighterGray: "#f1f1f4",
@@ -12,6 +20,13 @@ module.exports = {
         indigo: "#3f51b5",
         indigoDarken10: "#364495",
         indigoLighten80: "#b7c1f8",
+        logoOrange: "#e86c60",
+        logoBlue: "#43a6dd",
+        logoGray: "#e6e6e6",
+        logoDarkGray: "#b3b3b3",
+      },
+      fontSize: {
+        xxs: ["0.625rem", ".875rem"],
       },
     },
   },
