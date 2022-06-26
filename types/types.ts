01e1f05bf1b1752591b24f1a6ec22f0e132c97d9
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-06-27 01:43:07 +0430

    Relocate types to make it accessible to API

---
+++ types/types.ts
@@ -0,0 +1,9 @@
+export type BestsellerType = "FICTION" | "NON_FICTION";
+
+export type BriefBook = {
+  book_image: string;
+  title: string;
+  author: string;
+  primary_isbn10: string;
+  primary_isbn13: string;
+};
