901dbc3fab49da1f82f0d784cbed9214453597c4
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-06-28 02:04:38 +0430

    `BriefBook`  type is not needed outside `src`

--- types/types.ts
+++ types/types.ts
@@ -1,9 +1,2 @@
 export type BestsellerType = "FICTION" | "NON_FICTION";
 
-export type BriefBook = {
-  bookImage: string | null;
-  title: string;
-  author: string | null;
-  primaryISBN10: string;
-  primaryISBN13: string;
-};
