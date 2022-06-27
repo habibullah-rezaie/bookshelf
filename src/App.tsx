da1ca0ff069ee8b0f4db6b6b480769555929815b
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-06-28 02:12:41 +0430

    Render `HomeScreen` as the first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import DiscoverBooksScreen from "src/screens/DiscoverBooksScreen";
+import HomeScreen from "src/screens/HomeScreen";
 import LoginRegisterScreen from "src/screens/LoginRegisterScreen";
 
 function App() {
   const user = { password: "mypassword", username: "GUEST" };
   // const user = null;
-  return <>{user ? <DiscoverBooksScreen /> : <LoginRegisterScreen />}</>;
+  // return <>{user ? <DiscoverBooksScreen /> : <LoginRegisterScreen />}</>;
+  return <>{user ? <HomeScreen /> : <LoginRegisterScreen />}</>;
 }
 
 export default App;
