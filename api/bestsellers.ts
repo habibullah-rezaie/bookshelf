80d818190c402e7061de10d90089d9a36ac60c7b
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-06-28 01:51:39 +0430

    Move `triggerUpdate` function to `_util/bestsellers`

--- api/bestsellers.ts
+++ api/bestsellers.ts
@@ -4,6 +4,7 @@ import { fetchBestSellers } from "./_util/bestsellers";
 import { BriefBook } from "types/types";
 import db from "../database/db";
 import { checkForBestsellerUpdates } from "./_util/bestsellers";
+import { checkForBestsellerUpdates, triggerUpdate } from "./_util/bestsellers";
 
 export default async function handler(
   req: VercelRequest,
@@ -32,59 +33,6 @@ export default async function handler(
 
   // Finally, no matter what response with a list of bestsellers
   responsedWithListofBestsellers(resp);
-}
-
-async function triggerUpdate() {
-  const fictionBestsellers = await fetchBestSellers("FICTION");
-  const nonFictionBestsellers = await fetchBestSellers("NON_FICTION");
-
-  await db.bestsellerBook.deleteMany({});
-
-  await db.bestsellerBook.createMany({
-    data: [
-      // Add fiction bestsellers
-      ...fictionBestsellers.map(
-        ({
-          title,
-          author,
-          book_image,
-          primary_isbn10,
-          primary_isbn13,
-        }: BriefBook): Omit<
-          BestsellerBook,
-          "id" | "createdAt" | "updatedAt"
-        > => ({
-          author,
-          bookImage: book_image,
-          primaryISBN10: primary_isbn10,
-          primaryISBN13: primary_isbn13,
-          title,
-          type: "FICTION",
-        })
-      ),
-
-      // Add non-fiction bestsellers
-      ...nonFictionBestsellers.map(
-        ({
-          title,
-          author,
-          book_image,
-          primary_isbn10,
-          primary_isbn13,
-        }: BriefBook): Omit<
-          BestsellerBook,
-          "id" | "createdAt" | "updatedAt"
-        > => ({
-          author,
-          bookImage: book_image,
-          primaryISBN10: primary_isbn10,
-          primaryISBN13: primary_isbn13,
-          title,
-          type: "NON_FICTION",
-        })
-      ),
-    ],
-  });
 }
 
 async function responsedWithListofBestsellers(resp: VercelResponse) {
