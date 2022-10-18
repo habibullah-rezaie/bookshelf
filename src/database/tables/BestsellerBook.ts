7f017c9d954e3475e2a075a5f30a8ea69bea52fb
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-18 20:55:36 +0500

    Code selector function for BestsellerBook table

--- src/database/tables/BestsellerBook.ts
+++ src/database/tables/BestsellerBook.ts
@@ -1,3 +1,4 @@
+import supabase from "../db";
 
 export type BestsellerBook = {
 	id: string;
@@ -12,3 +13,41 @@ export type BestsellerBook = {
 };
 
 export type BestsellerType = "FICTION" | "NON_FICTION";
+
+export async function select(
+	query?: string,
+	options?: {
+		head?: boolean | undefined;
+		count?:
+			| "exact"
+			| "planned"
+			| "estimated"
+			| "exact"
+			| "planned"
+			| "estimated"
+			| null
+			| undefined;
+	}
+): Promise<BestsellerBook[]> {
+	if (!supabase) {
+		return Promise.reject("Something went wrong connecting to server.");
+	}
+
+	const finalQuery = query ? query : "*";
+	const {
+		data: bestsellers,
+		error,
+		status,
+	} = await supabase?.from("BestsellerBook").select(finalQuery);
+
+	if (error) {
+		return Promise.reject(error);
+	}
+
+	const responseIsOk = status >= 200 && status < 300;
+	if (!responseIsOk) {
+		return Promise.reject("Non 200 response code");
+	}
+
+	return Promise.resolve(bestsellers);
+}
