c75b2dd13aef6793bfa5870017403173b4fdfff7
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-19 23:22:33 +0500

    Format code

--- src/components/app/DiscoverBooksScreen/BooksList.tsx
+++ src/components/app/DiscoverBooksScreen/BooksList.tsx
@@ -4,62 +4,62 @@ import DetailedBookCard, { BriefBookCard } from "./BookCard";
 import SerachBookLoader from "./SearchBookLoader";
 
 function DetailedBooksList({
-  books,
-  status,
-  className = "",
+	books,
+	status,
+	className = "",
 }: {
-  books: Book[];
-  status: BaseComponentStatuses;
-  className?: string;
+	books: Book[];
+	status: BaseComponentStatuses;
+	className?: string;
 }) {
-  return (
-    <ul className="mt-2 flex flex-col justify-center space-y-3">
-      {status === "PENDING" && <SerachBookLoader />}
-      {status === "RESOLVED" && (
-        <>
-          {books?.length && books?.length > 0
-            ? books.map((book, i) => {
-                return (
-                  <li key={book.id}>
-                    <DetailedBookCard book={book} />
-                  </li>
-                );
-              })
-            : "Opps! nothing found :("}
-        </>
-      )}
-      {status === "IDLE" && (
-        <div>
-          <a href="#searchForBooks">Search results appear here!</a>
-        </div>
-      )}
-    </ul>
-  );
+	return (
+		<ul className="mt-2 flex flex-col justify-center space-y-3">
+			{status === "PENDING" && <SerachBookLoader />}
+			{status === "RESOLVED" && (
+				<>
+					{books?.length && books?.length > 0
+						? books.map((book, i) => {
+								return (
+									<li key={book.id}>
+										<DetailedBookCard book={book} />
+									</li>
+								);
+						  })
+						: "Opps! nothing found :("}
+				</>
+			)}
+			{status === "IDLE" && (
+				<div>
+					<a href="#searchForBooks">Search results appear here!</a>
+				</div>
+			)}
+		</ul>
+	);
 }
 
 export function BriefBooksList({
-  books,
-  className = "",
+	books,
+	className = "",
 }: {
-  books: BriefBook[];
-  className?: string;
+	books: BriefBook[];
+	className?: string;
 }) {
-  console.log(books);
-  return (
-    <ul className={`mt-2 flex flex-col justify-center space-y-3 ${className}`}>
-      <>
-        {books.length > 0
-          ? books.map((book) => {
-              return (
-                <li key={`${book.bookImage}`}>
-                  <BriefBookCard book={book} />
-                </li>
-              );
-            })
-          : `We're all caught up now.`}
-      </>
-    </ul>
-  );
+	console.log(books);
+	return (
+		<ul className={`mt-2 flex flex-row justify-center space-y-3 ${className}`}>
+			<>
+				{books.length > 0
+					? books.map((book) => {
+							return (
+								<li key={`${book.bookImage}`}>
+									<BriefBookCard book={book} />
+								</li>
+							);
+					  })
+					: `We're all caught up now.`}
+			</>
+		</ul>
+	);
 }
 
 export default DetailedBooksList;
