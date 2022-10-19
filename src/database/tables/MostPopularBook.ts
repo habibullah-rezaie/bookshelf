0735266bc247168da6ae07303792020d6fd128b6
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-19 23:31:11 +0500

    Export select methods specific to MostPopularBook table

--- src/database/tables/MostPopularBook.ts
+++ src/database/tables/MostPopularBook.ts
@@ -1,3 +1,11 @@
+import { PostgrestFilterBuilder } from "@supabase/postgrest-js";
+import supabase from "../db";
+import {
+	select as selectFromTable,
+	selectAndFilter,
+	SelectOptions,
+} from "../methods";
+
 // TODO: Check if this matches db
 export type MostPopularBook = {
 	title: string;
@@ -9,3 +17,24 @@ export type MostPopularBook = {
 export type PopularBookPeriod = "YEAR" | "MONTH" | "WEEK";
 
 const TABLE_NAME = "MostPopularBook";
+
+export async function select(
+	query?: string,
+	options?: SelectOptions
+): Promise<MostPopularBook[]> {
+	return selectFromTable<MostPopularBook>(TABLE_NAME, query || "", options);
+}
+
+export function selectAndFilterPopularBooks<MostPopularBook>(
+	query: string,
+	filterer: (
+		filterBuilder: PostgrestFilterBuilder<any>
+	) => PostgrestFilterBuilder<any>,
+	options?: SelectOptions
+): Promise<MostPopularBook[]> {
+	if (!supabase) {
+		return Promise.reject("Something went wrong connecting to server.");
+	}
+
+	return selectAndFilter<MostPopularBook>(TABLE_NAME, query, filterer, options);
+}
