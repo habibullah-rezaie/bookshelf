1d240ad2a6e9d7962e58e3895611f09dc5c7328b
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-28 12:58:09 +0300

    Change scrolling to smooth

--- src/screens/LoginRegisterScreen.tsx
+++ src/screens/LoginRegisterScreen.tsx
@@ -4,22 +4,23 @@ import { Container, Stack } from "src/components/lib/Layout";
 import Logo from "src/components/logo";
 
 function LoginRegisterScreen() {
-  return (
-    <Container className="flex justify-center text-textColor mobile-sm:text-sm">
-      <div className="w-min-max">
-        <Stack direction="vertical" className="mt-14 gap-9">
-          <Stack
-            direction="vertical"
-            className="justify-center gap-1 text-base mb-9"
-          >
-            <Logo className="w-16 m-auto" />
-            <h1 className="font-aldrich text-2xl">BookShelf</h1>
-          </Stack>
-          <Outlet />
-        </Stack>
-      </div>
-    </Container>
-  );
+	return (
+		<Container className="flex justify-center text-textColor mobile-sm:text-sm">
+			<div className="w-min-max">
+				<Stack direction="vertical" className="mt-14 gap-9">
+					<Stack
+						direction="vertical"
+						className="justify-center gap-1 text-base mb-9"
+					>
+						<Logo className="w-16 m-auto" />
+						<h1 className="font-aldrich text-2xl">BookShelf</h1>
+					</Stack>
+					{/* SignInPage or SignUpPage goes inside */}
+					<Outlet />
+				</Stack>
+			</div>
+		</Container>
+	);
 }
 
 export default LoginRegisterScreen;
