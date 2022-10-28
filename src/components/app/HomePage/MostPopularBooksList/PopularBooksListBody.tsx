4025e7f1ec1c99600850b988f84600533e37dc2a
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-28 12:49:30 +0300

    Use new card component

--- src/components/app/HomePage/MostPopularBooksList/PopularBooksListBody.tsx
+++ src/components/app/HomePage/MostPopularBooksList/PopularBooksListBody.tsx
@@ -1,21 +1,27 @@
 import { Resource } from "src/api/resource";
-import { BriefBooksList } from "src/components/app/DiscoverBooksScreen/BooksList";
 import { MostPopularBook } from "src/database/tables/MostPopularBook";
+import PopularBookCard from "./PopularBookCard";
 interface Props {
 	popularBookResource: Resource<MostPopularBook[]>;
 }
 function PopularBooksListBody({ popularBookResource }: Props) {
 	const books = popularBookResource.read();
 	return (
-		<BriefBooksList
-			books={books.map(({ author, title, imageURL, isbn }) => ({
-				title,
-				author,
-				bookImage: imageURL,
-				primaryISBN10: isbn.length === 10 ? isbn : "",
-				primaryISBN13: isbn.length === 13 ? isbn : "",
-			}))}
-		/>
+		<>
+			<ul className={`flex flex-col space-y-6`}>
+				<>
+					{books.length > 0
+						? books.map((book) => {
+								return (
+									<li className={``} key={book.id}>
+										<PopularBookCard book={book} />
+									</li>
+								);
+						  })
+						: `We're all caught up now.`}
+				</>
+			</ul>
+		</>
 	);
 }
 
