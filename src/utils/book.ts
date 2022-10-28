5f0327a1eddf5b03b39036818c51253f7632769c
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-28 12:44:40 +0300

    Add typo changes

--- src/utils/book.ts
+++ src/utils/book.ts
@@ -1,12 +1,12 @@
 export function getAuthorsSummary(authors: string[] = []) {
-  switch (authors.length) {
-    case 0:
-      return "Unkown";
-    case 1:
-      return authors[0];
-    case 2:
-      return `${authors[0]} and ${authors[1]}`;
-    default:
-      return `${authors[0]} and ${authors.length - 1}others`;
-  }
+	switch (authors.length) {
+		case 0:
+			return "Unkown Author";
+		case 1:
+			return authors[0];
+		case 2:
+			return `${authors[0]} & ${authors[1]}`;
+		default:
+			return `${authors[0]} & ${authors.length - 1} others`;
+	}
 }
