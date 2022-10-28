31b59bcb40e2b457c07a24e2b6ee96204cbc02d4
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-28 12:38:31 +0300

    Render horizontal or vertical cards

--- src/components/app/DiscoverBooksScreen/BooksList.tsx
+++ src/components/app/DiscoverBooksScreen/BooksList.tsx
@@ -1,6 +1,8 @@
-import React from "react";
 import { BaseComponentStatuses, Book, BriefBook } from "src/types/types";
-import DetailedBookCard, { BriefBookCard } from "./BookCard";
+import DetailedBookCard, {
+	ColBriefBookCard,
+	RowBriefBookCard,
+} from "./BookCard";
 import SerachBookLoader from "./SearchBookLoader";
 
 function DetailedBooksList({
@@ -40,19 +42,58 @@ function DetailedBooksList({
 export function BriefBooksList({
 	books,
 	className = "",
+	direction = "col",
+}: {
+	books: BriefBook[];
+	direction?: "row" | "col";
+	className?: string;
+}) {
+	return direction === "col" ? (
+		<BriefVerticalBookList books={books} className={className} />
+	) : (
+		<BriefHorizontalBookList books={books} className={className} />
+	);
+}
+
+export function BriefHorizontalBookList({
+	books,
+	className = "",
 }: {
 	books: BriefBook[];
 	className?: string;
 }) {
-	console.log(books);
 	return (
-		<ul className={`mt-2 flex flex-row justify-center space-y-3 ${className}`}>
+		<ul className={`bestseller-list h-[21.75rem] px-7 ${className}`}>
 			<>
 				{books.length > 0
 					? books.map((book) => {
 							return (
-								<li key={`${book.bookImage}`}>
-									<BriefBookCard book={book} />
+								<li className={`inline-block`} key={`${book.bookImage}`}>
+									<RowBriefBookCard book={book} />
+								</li>
+							);
+					  })
+					: `We're all caught up now.`}
+			</>
+		</ul>
+	);
+}
+
+export function BriefVerticalBookList({
+	books,
+	className = "",
+}: {
+	books: BriefBook[];
+	className?: string;
+}) {
+	return (
+		<ul className={`flex flex-col ${className}`}>
+			<>
+				{books.length > 0
+					? books.map((book) => {
+							return (
+								<li className={``} key={`${book.bookImage}`}>
+									<ColBriefBookCard book={book} />
 								</li>
 							);
 					  })
