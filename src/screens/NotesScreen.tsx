dbf54693e28115964b7b1daadb7db9cd7afd148e
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-28 12:58:27 +0300

    Create demo screens

---
+++ src/screens/NotesScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NotesScreen() {
+	return <div>NotesScreen</div>;
+}
+
+export default NotesScreen;
---
+++ src/screens/SettingsScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SettingsScreen() {
+	return <div>SettingsScreen</div>;
+}
+
+export default SettingsScreen;
