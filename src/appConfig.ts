d3c91aa6a44548fc353eaa8f457d77d6625f3f4e
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-02 16:10:29 +0300

    Type config
    for better auto complete

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,6 +1,17 @@
-export default Object.freeze({
+import { PopularBookPeriod } from "./database/tables/MostPopularBook";
+
+type ConfigType = {
+	DEFAULT_POPULAR_BOOKS_PERIOD: PopularBookPeriod;
+	DEFAULT_POPULAR_BOOKS_LIMIT: number;
+	DEFAULT_BESTSELLER_BOOKS_LIMIT: number;
+	DEFUALT_BOOK_IMG: string;
+};
+
+const config: ConfigType = Object.freeze({
 	DEFAULT_POPULAR_BOOKS_PERIOD: "WEEK",
 	DEFAULT_POPULAR_BOOKS_LIMIT: 10,
 	DEFAULT_BESTSELLER_BOOKS_LIMIT: 10,
 	DEFUALT_BOOK_IMG: "book.jpeg",
 });
+
+export default config;
