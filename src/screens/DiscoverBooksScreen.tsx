1b595916a3feb977d31c5e11a516cfe5e037e90d
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-03 07:17:12 +0300

    Use all new changes to get closer to prototype

--- src/screens/DiscoverBooksScreen.tsx
+++ src/screens/DiscoverBooksScreen.tsx
@@ -1,103 +1,93 @@
-import * as React from "react";
-import DetailedBooksList from "src/components/app/DiscoverBooksScreen/BooksList";
-import SearchBooks from "src/components/app/DiscoverBooksScreen/SearchBooks";
-import { Container, Stack } from "src/components/lib/Layout";
-import { SearchResult } from "src/types/DiscoverBooksScreenTypes";
-import { BaseComponentStatuses } from "src/types/types";
+import React from "react";
+import { useSearchBookBox } from "src/api/hooks/searchBooks";
+import SearchBox from "src/components/app/DiscoverBooksScreen/SearchBooks";
+import SearchResultsSection from "src/components/app/DiscoverBooksScreen/SearchResultsList";
+import ListHeading from "src/components/app/HomePage/ListHeader";
+import { Button } from "src/components/lib/Buttons/Buttons";
+import ScrollDirection from "src/components/lib/Icons/ScrollDirection";
+import { Stack } from "src/components/lib/Layout";
+import Pagination from "src/components/lib/Lists/Pagination";
 
-type dicoverBooksReducerAction = {
-	type: "RESOLVE" | "REJECT" | "SET_PENDING";
-	payload?: {
-		error?: Error;
-		data?: SearchResult;
-	};
-};
-
-type DiscoverBooksState = {
-	status: BaseComponentStatuses;
-	error?: Error;
-	data?: SearchResult;
-};
-
-function discoverBooksReducer(
-	state: DiscoverBooksState,
-	action: dicoverBooksReducerAction
-): DiscoverBooksState {
-	switch (action.type) {
-		case "SET_PENDING": {
-			return { status: "PENDING", data: undefined, error: undefined };
-		}
-		case "RESOLVE": {
-			return {
-				status: "RESOLVED",
-				data: action.payload?.data,
-				error: undefined,
-			};
-		}
-		case "REJECT": {
-			return {
-				status: "REJECTED",
-				error: action.payload?.error,
-				data: undefined,
-			};
-		}
-		default:
-			return state;
-	}
-}
-
-const discoverBooksDefaultState: DiscoverBooksState = {
-	status: "IDLE",
-};
+// TODO: Show sort bar on every State
+// TODO:  Show loader
 function DiscoverBooksScreen() {
-	const [state, dispatch] = React.useReducer<typeof discoverBooksReducer>(
-		discoverBooksReducer,
-		discoverBooksDefaultState
-	);
+	const {
+		data,
+		error,
+		isError,
+		isLoading,
+		isSuccess,
+		query,
+		search,
+		filters,
+		page,
+		setPage,
+		hasNextPage,
+	} = useSearchBookBox(10);
 
-	const setResolved = React.useCallback((data: SearchResult) => {
-		dispatch({ type: "RESOLVE", payload: { data } });
-	}, []);
+	const divRef = React.useRef<HTMLDivElement>(null);
 
-	const setRejected = React.useCallback((error: Error) => {
-		dispatch({ type: "REJECT", payload: { error } });
-	}, []);
-
-	const setPending = React.useCallback(() => {
-		dispatch({ type: "SET_PENDING" });
-	}, []);
-
-	// TODO: Add pagination
-	// TODO: Add result count
+	function handleSortChange(sortBy: "relevance" | "newest") {
+		search(query, { ...filters, sortBy });
+	}
 
 	return (
-		<div className="w-full h-full relative px-2 text-sm">
-			<Container className={`p1 pt-3 mt-14`}>
-				<Stack
-					direction="horizontal"
-					gap={12}
-					className="grid-cols-4 sm:grid-cols-8 md:grid-cols-10 lg:grid-cols-12"
+		<div className="px-7">
+			<Stack
+				ref={divRef}
+				direction="horizontal"
+				gap={12}
+				className="relative grid-cols-4 sm:grid-cols-8 md:grid-cols-10 lg:grid-cols-12"
+			>
+				<main
+					className="col-span-4 col-start-1 sm:col-span-6 sm:col-start-2 md:col-span-8 md:col-start-2 lg:col-span-10 lg:col-start-2"
+					id="discover"
 				>
-					<main
-						className="col-span-4 col-start-1 sm:col-span-6 sm:col-start-2 md:col-span-8 md:col-start-2 lg:col-span-10 lg:col-start-2"
-						id="discover"
-					>
-						<Stack direction="vertical" gap={6} className="max-w-full">
-							<h2 className="text-center">Discover Books Here</h2>
-							<SearchBooks
-								status={state.status}
-								setPending={setPending}
-								setRejected={setRejected}
-								setResolved={setResolved}
+					<ListHeading className="mt-6 mb-5">Search Books</ListHeading>
+					<div className="relative pb-9">
+						<SearchBox
+							onSearch={search}
+							query={query}
+							filters={filters}
+							isError={isError}
+							isLoading={isLoading}
+						/>
+					</div>
+
+					{isSuccess && data.totalItems && data.items instanceof Array && (
+						<>
+							<SearchResultsSection
+								data={data}
+								onSortChange={handleSortChange}
+								selectedSorting={filters.sortBy || "relevance"}
 							/>
-							<DetailedBooksList
-								books={state.data?.items || []}
-								status={state.status}
+
+							<Button
+								variant="plain"
+								className="fixed bottom-28 right-10 w-10 h-10 z-10 flex items-center justify-center rounded-[100%] bg-baseBlack text-white"
+								title="Scroll to Search"
+								onClick={() => divRef.current?.scrollIntoView()}
+							>
+								<ScrollDirection direction="UP" className="w-8 h-8" />
+							</Button>
+
+							<Pagination
+								page={page}
+								pageSize={10}
+								totalItems={data.totalItems}
+								hasNextPage={hasNextPage}
+								onNextPage={() => setPage((page) => page + 1)}
+								onPreviousPage={() =>
+									setPage((page) => {
+										console.log("changed paged");
+										return page > 0 ? page - 1 : page;
+									})
+								}
 							/>
-						</Stack>
-					</main>
-				</Stack>
-			</Container>
+						</>
+					)}
+				</main>
+			</Stack>
 		</div>
 	);
 }
