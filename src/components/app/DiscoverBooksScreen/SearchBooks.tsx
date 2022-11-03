e400aadfd72c821d4a39484569d0e5ea7b7ef134
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-03 07:16:27 +0300

    Use new react-query based searching hook

--- src/components/app/DiscoverBooksScreen/SearchBooks.tsx
+++ src/components/app/DiscoverBooksScreen/SearchBooks.tsx
@@ -1,76 +1,38 @@
+import React, { FormEvent } from "react";
+import { FaSearch, FaSpinner } from "react-icons/fa";
+import { VscSettings } from "react-icons/vsc";
+import { SearchHandler } from "src/api/hooks/searchBooks";
 import { Button } from "src/components/lib/Buttons/Buttons";
-import { Form, Input } from "src/components/lib/Forms";
-import { Stack } from "src/components/lib/Layout";
-import React, { FormEvent, useEffect, useState } from "react";
-import { FaFilter, FaSearch, FaSpinner } from "react-icons/fa";
-import { BaseComponentStatuses } from "src/types/types";
-import { isDeepStrictEqual } from "src/utils/utils";
+import { Input } from "src/components/lib/Forms";
+import { SearchFilters } from "src/types/DiscoverBooksScreenTypes";
+import FilterModalLander from "./FilterButton";
 
-function useSearchWithFilters({
-	setResolved,
-	setRejected,
-	setPending,
+export type FiltersSetter = React.Dispatch<React.SetStateAction<SearchFilters>>;
+
+function SearchRow({
+	onSearch,
+	filters: finalFilters,
+	query: finalQuery,
+	isLoading,
+	isError,
 }: {
-	setResolved(result: SearchResult): void;
-	setRejected: (error: any) => void;
-	setPending: () => void;
-}) {
-	const [query, setQuery] = useState("");
-	const [filters, setFilters] = React.useState<SearchFilters>({});
-	const [filtersSubmitted, setFiltersSubmitted] = React.useState(false);
-
-	const search = React.useCallback(
-		function search() {
-			setPending();
-			searchBook(query, filters).then(
-				(result) => {
-					setResolved(result);
-				},
-				(error) => {
-					setRejected(error);
-				}
-			);
-		},
-		[filters, query, setPending, setRejected, setResolved]
-	);
-
-	const filtersRef = React.useRef(filters);
-	useEffect(() => {
-		if (filtersSubmitted && !isDeepStrictEqual(filtersRef.current, filters)) {
-			filtersRef.current = filters;
-			search();
-			console.log("searched");
-		}
-	}, [filters, filtersSubmitted, search]);
-
-	return {
-		search,
-		query,
-		setQuery,
-		filters,
-		setFilters,
-		setFiltersSubmitted,
-	};
-}
-
-function SearchBooks({
-	status,
-	setResolved,
-	setPending,
-	setRejected,
-}: {
-	status: BaseComponentStatuses;
-	setResolved(result: SearchResult): void;
-	setRejected: (error: any) => void;
-	setPending: () => void;
+	query: string;
+	filters: SearchFilters;
+	onSearch: SearchHandler;
+	isLoading: boolean;
+	isError: boolean;
 }) {
 	const [filterModalOpen, setFilterModalOpen] = React.useState(false);
-	const { search, query, setQuery, setFilters, setFiltersSubmitted } =
-		useSearchWithFilters({
-			setRejected,
-			setResolved,
-			setPending,
-		});
+
+	const [query, setQuery] = React.useState<string>(finalQuery);
+	const [filters, setFilters] = React.useState<SearchFilters>(finalFilters);
+	const [filtersSubmitted, setFiltersSubmitted] = React.useState(false);
+
+	React.useEffect(() => {
+		if (filtersSubmitted) {
+			onSearch(query, filters);
+		}
+	}, [filters, filtersSubmitted, onSearch, query]);
 
 	function closeFiltersModal() {
 		setFilterModalOpen(false);
@@ -79,55 +41,64 @@ function SearchBooks({
 	function handleSubmit(ev: FormEvent<HTMLFormElement>) {
 		ev.preventDefault();
 
-		search();
+		// Either newly submitted filters or original filters
+		onSearch(query, filtersSubmitted ? filters : finalFilters);
 	}
+
 	return (
 		<>
-			<Form className="" onSubmit={handleSubmit}>
-				<Stack
-					direction="horizontal"
-					className="rounded-md ring-1 ring-opacity-40 ring-logoBlue focus-within:ring-2 focus-within:ring-opacity-20  focus-within:ring-logoBlue focus-within:border-[1px] focus-within:border-logoBlue max-w-full"
-				>
+			<form
+				className="w-full h-10 flex flex-row items-center justify-center space-x-2"
+				onSubmit={handleSubmit}
+			>
+				<div className="w-64 h-9 flex flex-row ring-1 ring-opacity-40 ring-baseBlack focus-within:ring-2 focus-within:ring-opacity-20  focus-within:ring-baseBlack focus-within:border-[1px] focus-within:boring-baseBlack max-w-full transition-all duration-75">
 					<label htmlFor="searchInput" className="sr-only">
 						search for books
 					</label>
+
 					<Button
 						variant="plain"
-						type="button"
-						className="px-1.5 bg-transparent rounded-sm rounded-l-none hover:bg transition-all duration-150 text-logoDarkGray hover:text-gray-700 active:text-gray-700"
-						onClick={() => setFilterModalOpen(true)}
+						type="submit"
+						className="w-fit px-1.5 text-center text-darkGray transition-all duration-150"
 					>
-						<FaFilter className="" />
+						{/* TODO: disable button */}
+						{/* TODO: turn red on error */}
+						{isLoading ? (
+							<FaSpinner className="animate-spin" />
+						) : (
+							<FaSearch className="" />
+						)}
 					</Button>
 
 					<Input
 						role="searchbox"
 						id="searchInput"
-						className="w-48"
+						className="flex-1"
+						placeholder="Search"
 						value={query}
 						onChange={(ev) => setQuery(ev.target.value)}
-						autoFocus
+						// autoFocus
 					/>
-					<Button
-						variant="plain"
-						type="submit"
-						className="px-1.5 bg-logoBlue rounded-sm rounded-l-none hover:bg transition-all duration-150"
-					>
-						{/* TODO: disable button */}
-						{/* TODO: turn red on error */}
-						{status === "PENDING" ? (
-							<FaSpinner className="animate-spin" />
-						) : (
-							<FaSearch className="text-white" />
-						)}
-					</Button>
-				</Stack>
-			</Form>
+				</div>
+
+				<Button
+					className="flex flex-row justify-center items-center w-14 h-9 col-end-12 px-1 bg-baseBlack text-white  rounded-sm rounded-l-none hover:bg transition-all duration-150 hover:ring-1 hover:ring-baseBlack  active:ring-1  active:ring-baseBlack"
+					variant="plain"
+					type="button"
+					onClick={() => setFilterModalOpen(true)}
+				>
+					<VscSettings className="h-4 -rotate-90" />
+				</Button>
+			</form>
+
+			<FilterModalLander
+				closeFiltersModal={closeFiltersModal}
+				filterModalOpen={filterModalOpen}
+				setFilters={setFilters}
+				setFiltersSubmitted={setFiltersSubmitted}
+			/>
 		</>
-
-
-
 	);
 }
 
-export default SearchBooks;
+export default SearchRow;
