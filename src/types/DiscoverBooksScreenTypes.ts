b72d31b3904dfb988ed269152a143d06de8b0434
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-03 07:17:58 +0300

    Include title as a filter

--- src/types/DiscoverBooksScreenTypes.ts
+++ src/types/DiscoverBooksScreenTypes.ts
@@ -1,12 +1,13 @@
 export type BookLanguage = { English: string; alpha2: string };
 export type SearchFilters = {
-  author?: string;
-  publisher?: string;
-  downloadable?: boolean;
-  isbn?: string;
-  sortBy?: "relevance" | "newest";
-  language?: string;
-  category?: string;
+	title?: string;
+	author?: string;
+	publisher?: string;
+	downloadable?: boolean;
+	isbn?: string;
+	sortBy?: "relevance" | "newest";
+	language?: string;
+	category?: string;
 };
 
 export type setFilters = React.Dispatch<React.SetStateAction<SearchFilters>>;
