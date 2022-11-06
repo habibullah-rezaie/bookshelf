cbb0b2c0a76e41cd5b509f5f3d8edc2f139da202
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-06 10:43:01 +0300

    Replace usage of img with `ImgWithLoader`

--- src/components/app/BookCards/HorizontalBookCard.tsx
+++ src/components/app/BookCards/HorizontalBookCard.tsx
@@ -1,6 +1,8 @@
 import { BsStarFill } from "react-icons/bs";
 import { VscBookmark } from "react-icons/vsc";
 import appConfig from "src/appConfig";
+import ImgWithLoader from "src/components/lib/Img/ImgWithLoader";
+import SquareLoader from "src/components/lib/Loaders/SquareLoader";
 import { BasicBookInfo } from "src/types/types";
 import { getAuthorsSummary } from "src/utils/book";
 import { trimTextWithElepsis } from "src/utils/utils";
@@ -28,13 +30,14 @@ function HorizontalBookCard({
 
 	return (
 		<section className="w-full h-28 max-h-28 font-poppins flex flex-row justify-between rounded-md border-[1px] border-x-0 drop-shadow-sm">
-			<div className="h-full w-max">
-				<img
-					className="rounded-md h-28 w-auto"
-					src={bookImage || appConfig.DEFUALT_BOOK_IMG}
-					alt={`${title}'s cover`}
-				/>
-			</div>
+			<ImgWithLoader
+				className="rounded-md max-h-[6.875rem] max-w-[4.6rem] object-contain flex-1"
+				src={bookImage || appConfig.DEFUALT_BOOK_IMG}
+				width={10}
+				height={15}
+				alt={`${title}'s cover`}
+				Loader={<SquareLoader className="rounded-md" />}
+			/>
 			<div className="flex-1 pt-4 pl-6 pr-4 rounded-md rounded-l-none">
 				<div className="space-y-2">
 					<h1 className="text-sm  font-semibold" title={title}>
