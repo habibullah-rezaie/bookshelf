bee0cbc9fcdb5bb2ff62918ca4b53ae8fac18b48
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-06 10:40:39 +0300

    Create a template square loader

---
+++ src/components/lib/Loaders/SquareLoader.tsx
@@ -0,0 +1,7 @@
+export default function SquareLoader({ className = "" }) {
+	return (
+		<div
+			className={`w-full h-full bg-[#D9D9D9] animate-pulse flex items-center justify-center ${className}`}
+		></div>
+	);
+}
