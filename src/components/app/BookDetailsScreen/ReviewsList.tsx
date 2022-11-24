e1729325b2263a114876b2beb5040050a2c991df
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-24 16:31:59 +0300

    FIX:list of books thinner that search bar

--- src/components/app/BookDetailsScreen/ReviewsList.tsx
+++ src/components/app/BookDetailsScreen/ReviewsList.tsx
@@ -30,7 +30,7 @@ export function ReviewsListBody({
 	children?: React.ReactNode;
 }) {
 	return (
-		<ul className="flex flex-col mt-4 space-y-4">
+		<ul className="w-full flex flex-col mt-4 space-y-4">
 			{bookReviews &&
 				bookReviews.map((review) => {
 					return (
