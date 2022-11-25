1a8a83ccb0f6d67df45d31432e82382356fce202
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-25 20:06:19 +0300

    Make header be fixed to the top if wanted

--- src/components/lib/Header/Header.tsx
+++ src/components/lib/Header/Header.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-type Props = React.ComponentProps<"header">;
+type Props = React.ComponentProps<"header"> & { sticky?: boolean };
 
 const Header = React.forwardRef(function Header(
-	{ children, className = "" }: Props,
+	{ children, className = "", sticky = false }: Props,
 	ref?: React.ForwardedRef<HTMLDivElement>
 ) {
 	return (
 		<header
-			className={`z-50 w-full h-fit max-h-20 px-7 pt-10 flex flex-row items-center justify-between ${className}`}
+			className={`z-50 w-full h-fit max-h-20 px-7 pt-10 flex flex-row items-center justify-between ${
+				sticky === true ? `fixed top-0 left-0` : ""
+			} ${className}`}
 			ref={ref}
 		>
 			{children}
