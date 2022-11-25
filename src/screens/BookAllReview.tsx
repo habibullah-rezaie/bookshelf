9fbe30560006ef85f7b77f526cbfda36a0a7367a
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-25 21:48:43 +0300

    Either navigate home or back

--- src/screens/BookAllReview.tsx
+++ src/screens/BookAllReview.tsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import BookAllReviewsMain from "src/components/app/AllReviews/BookAllReviewsMain";
 import HeaderWithBackAndShare from "src/components/lib/Header/HeaderWithBackAndShare";
+import { goBackOrHome } from "src/utils/utils";
 
 function BookAllReviews() {
 	const navigate = useNavigate();
 	return (
 		<div>
-			<HeaderWithBackAndShare onBackClick={() => navigate(-1)} />
+			<HeaderWithBackAndShare onBackClick={() => goBackOrHome(navigate)} />
 			<div className="pt-20">
 				<BookAllReviewsMain />
 			</div>
