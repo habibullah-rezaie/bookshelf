fc76f6a6aea390541c2c55a6f066d78451302d12
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-26 15:20:05 +0300

    Show rating and review counts

--- src/components/app/BookCards/RatingPublishedDateRow.tsx
+++ src/components/app/BookCards/RatingPublishedDateRow.tsx
@@ -1,14 +1,49 @@
+import { useParams } from "react-router-dom";
+import { useRatingStats } from "src/api/hooks/userBook";
+import { useReviewsOnBook } from "src/api/hooks/userReview";
+import { formatNumberSuffix as getNumText } from "src/utils/utils";
 import { RatingStars } from "../BookDetailsScreen/MyReview";
+function RatingPublishedDateRow({ averageRating = 0 }) {
+	const { bookId } = useParams();
 
-function RatingPublishedDateRow({ dateString = "", averageRating = 0 }) {
-	return dateString || averageRating > 0 ? (
-		<div className="flex flex-col text-xxs font-poppins text-baseBlack opacity-80 space-y-6">
-			{dateString && <div>Published {dateString}</div>}
+	const { data: ratingStats } = useRatingStats(bookId || "");
+	const ratingsCount =
+		ratingStats && ratingStats.total != null ? ratingStats.total : 0;
+	const { data: reviewsOnBook } = useReviewsOnBook(bookId || "");
+	const reviewsCount = reviewsOnBook
+		? reviewsOnBook.pages.length > 0
+			? reviewsOnBook.pages[0].count ?? 0
+			: 0
+		: 0;
+
+	return (
+		<div className="flex flex-col font-poppins text-baseBlack opacity-80 mt-6">
 			<div>
 				<RatingStars rating={averageRating} className="text-2xl" />
+				<div className="flex flex-row font-poppins text-baseBlack mt-9">
+					<RatingsCount ratingsCount={ratingsCount} />
+					<ReviewsCount reviewsCount={reviewsCount} />
+				</div>
 			</div>
 		</div>
-	) : null;
+	);
 }
 
 export default RatingPublishedDateRow;
+function ReviewsCount({ reviewsCount = 0 }: { reviewsCount: number }) {
+	return (
+		<div className="flex flex-col space-y-1 pl-9">
+			<div className="font-extrabold text-2xl">{getNumText(reviewsCount)}</div>
+			<div className="font-normal text-opacity-80 text-xs">Reviews</div>
+		</div>
+	);
+}
+
+function RatingsCount({ ratingsCount = 0 }: { ratingsCount: number }) {
+	return (
+		<div className="flex flex-col space-y-1 pr-9 border-r-[1px] border-baseColor border-opacity-80">
+			<div className="font-extrabold text-2xl">{getNumText(ratingsCount)}</div>
+			<div className="font-normal text-opacity-80 text-xs">Ratings</div>
+		</div>
+	);
+}
