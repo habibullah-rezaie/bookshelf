38739aef41e85b544e5ff1d8d968c42e7cef0ecb
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-26 17:08:53 +0300

    Make bottom bar more flexible

--- src/components/lib/BottomBar.tsx
+++ src/components/lib/BottomBar.tsx
@@ -6,7 +6,7 @@ type props = React.PropsWithChildren<{ className?: string }>;
 function BottomBar({ children, className = "" }: props) {
 	return (
 		<div
-			className={`fixed bottom-0 z-10 left-0 w-full h-16 py-5 bg-[#F2F2F2] shadow-xl ${className}`}
+			className={`fixed bottom-0 z-10 left-0 w-full bg-[#F2F2F2] shadow-xl ${className}`}
 		>
 			{children}
 		</div>
