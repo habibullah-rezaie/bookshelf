41665489ba3d0bbd67a272cdbf062a62b3ab7238
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-27 13:00:31 +0300

    use `uneInfiniteQuery` to confirm to provide consisting caching with populars page

--- src/api/hooks/mostPopular.ts
+++ src/api/hooks/mostPopular.ts
@@ -1,30 +1,19 @@
 import { useQueries, useQuery, useQueryClient } from "@tanstack/react-query";
-import appConfig from "src/appConfig";
 import { PopularBookPeriod } from "src/database/tables/MostPopularBook";
 import {
 	popularBookQueryBuilder,
 	popularBooksListQueryBuilder,
 } from "../queries/mostPopular";
 
-export function useMostPopularBooks(
-	period: PopularBookPeriod,
-	pageSize: number = 10,
-	page: number = 1
-) {
-	let queryClient = useQueryClient();
-	return useQuery(popularBookQueryBuilder(period, queryClient, page, pageSize));
+export function useMostPopularBooks(period: PopularBookPeriod) {
+	return useQuery(popularBookQueryBuilder(period));
 }
 
-export function usePrefetchMostPopular(
-	period: PopularBookPeriod,
-	page: number = 1,
-	pageSize: number = appConfig.DEFAULT_POPULAR_BOOKS_LIMIT
-) {
+export function usePrefetchMostPopular(period: PopularBookPeriod) {
 	let queryClient = useQueryClient();
 
-	queryClient.prefetchQuery({
-		...popularBookQueryBuilder(period, queryClient, page, pageSize),
-		staleTime: 1000 * 30,
+	queryClient.prefetchInfiniteQuery({
+		...popularBookQueryBuilder(period),
 	});
 }
 
