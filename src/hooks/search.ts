e966898cad4d592aec6bee4b6d2116aa164a76ad
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-27 12:33:40 +0300

    Create a search by param hook for bestsellers

--- src/hooks/search.ts
+++ src/hooks/search.ts
@@ -1,8 +1,10 @@
 import React, { useCallback } from "react";
 import { useSearchParams } from "react-router-dom";
+import { useBestsellerSearch } from "src/api/hooks/bestsellers";
 import { useSearchBookInfiniteLoading } from "src/api/hooks/searchBooks";
 import { useBookReviewSearch } from "src/api/hooks/userReview";
 import { ReviewFilters } from "src/components/app/AllReviews/BookAllReviewsMain";
+import { BestsellerFilters } from "src/database/tables/BestsellerBook";
 import { SearchFilters } from "src/types/DiscoverBooksScreenTypes";
 import { generateSearchParams } from "src/utils/list";
 
@@ -97,6 +99,59 @@ export function useSearchReviewByParam(bookId: string) {
 	const onSearch = useCallback(
 		(query: string, filters: ReviewFilters) => {
 			let param = generateSearchParams<ReviewFilters>(query, filters);
+
+			setSearchParams(param);
+		},
+		[setSearchParams]
+	);
+
+	return {
+		search: onSearch,
+		queryEnabled,
+		setQueryEnabled,
+		parseError,
+		queryObj,
+		...rest,
+	};
+}
+
+export function useSearchBestsellerByParam() {
+	const [queryEnabled, setQueryEnabled] = React.useState(false);
+	const [parseError, setParseError] = React.useState("");
+	const [searchParams, setSearchParams] = useSearchParams();
+	const { search, queryObj, ...rest } = useBestsellerSearch();
+
+	React.useEffect(() => {
+		try {
+			const query = searchParams.get("q");
+			const filters = searchParams.get("filters");
+			if (!query && !filters) {
+				return;
+			}
+
+			const decodedQuery = decodeURIComponent(query || "");
+			const decodedFilters = JSON.parse(decodeURIComponent(filters || "{}"));
+
+			console.log("decodedFilters", decodedFilters);
+
+			if (!queryEnabled) {
+				console.log("notqueried", filters, query);
+				setQueryEnabled(true);
+			}
+
+			console.log("decoded", decodedQuery, decodedFilters);
+			search(decodedQuery, decodedFilters);
+		} catch (err) {
+			if (process.env.NODE_ENV !== "production") {
+				console.log(err);
+			}
+			setParseError("Something Unexpected Happened");
+		}
+	}, [searchParams, search, queryEnabled]);
+
+	const onSearch = useCallback(
+		(query: string, filters: BestsellerFilters) => {
+			let param = generateSearchParams<BestsellerFilters>(query, filters);
 
 			setSearchParams(param);
 		},
