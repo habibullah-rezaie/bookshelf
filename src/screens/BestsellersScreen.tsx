1b4c0c14abbba3c507edc0a87e17cfba3ea5ae8c
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-11-27 12:36:02 +0300

    Create BestsellersScreen.tsx

---
+++ src/screens/BestsellersScreen.tsx
@@ -0,0 +1,16 @@
+import { useNavigate, useParams } from "react-router-dom";
+import BestsellersScreenMain from "src/components/app/AllBestsellers/BestsellersScreenMain";
+import HeaderWithBackAndShare from "src/components/lib/Header/HeaderWithBackAndShare";
+import { goBackOrHome } from "src/utils/utils";
+
+function BestsellersScreen() {
+	const navigate = useNavigate();
+	return (
+		<div>
+			<HeaderWithBackAndShare onBackClick={() => goBackOrHome(navigate)} />
+			<BestsellersScreenMain />
+		</div>
+	);
+}
+
+export default BestsellersScreen;
